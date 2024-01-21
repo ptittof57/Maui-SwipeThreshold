b5ec8eb40fc800ea996f4cb3415a8796372479e7
ragtop <classicdashboard@gmail.com>
2024-01-21 11:11:21 +0100

    Add project files.

---
+++ SwipeThreshold/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SwipeThreshold
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SwipeThreshold/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace SwipeThreshold
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SwipeGestureRecognizer_Swiped(object sender, SwipedEventArgs e)
+        {
+            Console.WriteLine("Swiped");
+        }
+    }
+}
